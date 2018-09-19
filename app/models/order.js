b1488b3809e06f67f9a58b370990b3c5a49d33a2
mdipietr0 <dipietro.michael@gmail.com>
2018-09-19 13:32:55 -0400

    Create Order model in mongoose
    fields: data, status, line_item, user_id
    line_item is an array of LineItem objects
    LineItem still needs to be defined
    
    Co-authored-by: Simon Pringle-Wallace @SimonPringleWallace

---
+++ app/models/order.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const LineItem = require('./line-item')
+
+const orderSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+    // Add validation for possible value set
+  },
+  line_item: [LineItem],
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Order', orderSchema)
